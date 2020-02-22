262004d1a306cac1ca6772fded93c326b530c97f
yodatk <37527725+yodatk@users.noreply.github.com>
2020-02-22 17:48:53 +0200

    Initial commit

---
+++ TG_To_Do_List/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TgToDoListConfig(AppConfig):
+    name = 'TG_To_Do_List'
---
+++ TG_To_Do_List/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+MAX_LENGTH = 200
+
+
+# Create your models here.
+
+class Todo(models.Model):
+    added_date = models.DateTimeField()
+    text = models.CharField(max_length=MAX_LENGTH)
